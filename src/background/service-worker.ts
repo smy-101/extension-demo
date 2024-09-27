20d00434f3ed1e02539505d1ecb3b85bb617e4a2
smy-101 <shimy1011@foxmail.com>
2024-09-27 10:22:25 +0800

    添加跳转到options页面的菜单

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,1 +1,16 @@
+//用于右键菜单跳转到options页面
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "settings",
+    title: "扩展设置",
+    contexts: ["action"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === 'settings') {
+    chrome.runtime.openOptionsPage();
+  }
+});
+
 console.log('this is background service worker file')
